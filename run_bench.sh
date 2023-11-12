796322940a9ed0c520cf85ed953ab1afa1c5686f
Shunting Zhang <shunting@fb.com>
2023-11-12 10:31:10 -0800

    add a command for TB in run_bench.sh

--- run_bench.sh
+++ run_bench.sh
@@ -1,1 +1,5 @@
+# HF
 python benchmarks/dynamo/huggingface.py --backend inductor --amp --performance --only BertForMaskedLM --training
+
+# TB
+python benchmarks/dynamo/torchbench.py --backend inductor --amp --performance --only moco --training
