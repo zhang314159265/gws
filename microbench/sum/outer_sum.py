3c9b7f2de1f9255b1a06508d5c26d38018f22f08
Shunting Zhang <shunting@fb.com>
2023-12-07 22:13:09 -0800

    add a microbench for outer reduction

--- microbench/sum/outer_sum.py
+++ microbench/sum/outer_sum.py
@@ -1,14 +1,11 @@
-# Benchmark inner reduction
-# Example outputs:
-#   (1024, 1048576), 2.674208 ms, 1.606071 tbgs
-#   (1024, 1048573), 3.332944 ms, 1.288637 tbgs
-#   (1024, 1048579), 3.323552 ms, 1.292286 tbgs
+# Benchmark outer reduction
 
 import torch
 from torch._inductor.utils import do_bench
 from torch._inductor import config
+from torch._dynamo.testing import rand_strided
 
-# config.benchmark_kernel = True
+config.benchmark_kernel = True
 
 torch.set_default_device("cuda")
 
@@ -18,11 +15,14 @@ def f(x):
 
 for M, N in (
     (1024, 1024 * 1024),
-    (1024, 1024 * 1024 - 3), # 1.25x slow down
-    (1024, 1024 * 1024 + 3),
+    (1024, 1024 * 1024 - 3), # around 1x
+    (1024, 1024 * 1024 + 3), # around 1x
+    (1024 - 3, 1024 * 1024), # around 1.16x slower
+    (1024 + 3, 1024 * 1024), # around 1.16x slower
 ):
-    x = torch.rand(M, N)
+    x = rand_strided((M, N), (1, M), device="cuda")
     ms = do_bench(lambda: f(x))
     total_bytes = M * N * 4
     print(f"({M}, {N}), {ms:3f} ms, {total_bytes / ms / 1e9:3f} tbgs")
+
 print("bye")
