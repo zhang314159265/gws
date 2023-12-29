9ac3e3c5d37f956805919d891566b6fb2f7f0b32
Shunting Zhang <shunting@fb.com>
2023-12-28 22:38:29 -0800

    my bert model: pass training accuracy test

--- model/bert.py
+++ model/bert.py
@@ -5,10 +5,20 @@
 import sys
 sys.path.append(".")
 
+class args:
+    training = True
+
+    # args no need to change
+    iterations = 2
+
+class bench_state:
+    optimizer = None
+    model_iter_fn = None
+
 import torch
 from transformers import BertForMaskedLM as model_cls
 from model.bert_model import MyBertModel
-from torch._dynamo.testing import reset_rng_state
+from torch._dynamo.testing import reset_rng_state, collect_results
 from torch._dynamo.utils import same
 
 def download_model(model_cls, config):
@@ -21,7 +31,6 @@ model = download_model(model_cls, config)
 device = "cuda"
 dtype = torch.float32
 model = model.to(device, dtype=dtype)
-# batch_size = 16 # batch size is 1 for accuracy of HF
 batch_size = 1
 seq_length = 512
 vocab_size = model.config.vocab_size
@@ -38,16 +47,61 @@ for attr in dir(config):
     if "drop" in attr and isinstance(getattr(config, attr), float):
         setattr(config, attr, 1e-30)
 
-model.eval()
-ref_output = model(**input_dict)
+def model_iter_fn_fwd(model, args, collect_outpus=True):
+    return model(**args)
+
+def model_iter_fn_fwd_bwd(model, args, collect_outputs=True):
+    bench_state.optimizer.zero_grad(True)
+    pred = model(**args)
+
+    # pred[0] works for MaskedLMOutput but does not work for a reguard dict retuend
+    # in my model
+    loss = pred[next(iter(pred))]
+    loss.backward()
+    bench_state.optimizer.step()
+    if collect_outputs:
+        results = collect_results(model, pred, loss, args)
+
+        # key may be different due to simplification of module tree
+        results[2] = tuple(results[2].values())
+        results[3] = tuple(results[3].values())
+        return results
+
+def init_optimizer(params):
+    if args.training:
+        bench_state.optimizer = torch.optim.Adam(params, lr=0.01, capturable=True, foreach=True)
+    else:
+        bench_state.optimizer = None
+
+def run_n_iterations(mod, inputs):
+    n = args.iterations
+    for _ in range(n - 1):
+        bench_state.model_iter_fn(mod, inputs, collect_outputs=False)
+    return bench_state.model_iter_fn(mod, inputs, collect_outputs=True)
+
+bench_state.model_iter_fn = model_iter_fn_fwd_bwd if args.training else model_iter_fn_fwd
+if args.training:
+    model.train()
+else:
+    model.eval()
+init_optimizer(model.parameters())
+
+# This is important to make sure dropout behavior is deterministic for training.
+reset_rng_state()
+ref_output = run_n_iterations(model, input_dict)
 
 reset_rng_state()
-my_model = MyBertModel().to(device="cuda").eval()
-act_output = my_model(**input_dict)
+my_model = MyBertModel().to(device="cuda")
+if args.training:
+    my_model.train()
+else:
+    my_model.eval()
+init_optimizer(my_model.parameters())
+reset_rng_state()
+act_output = run_n_iterations(my_model, input_dict)
 
 if not same(ref_output, act_output):
     breakpoint()
 
 assert same(ref_output, act_output)
-
 print("bye")
