3397fcb280e5374b38df9ea1c748a03cef398435
zhang314159265 <shunting@fb.com>
2024-02-04 01:03:10 -0800

    tritoncc: pointwise add

--- findsym.sh
+++ findsym.sh
@@ -14,7 +14,10 @@ shift
 sym=$1
 shift
 
-for lib in `ls $dir/*.so $dir/*.a`; do
+# this is not recursive
+# for lib in `ls $dir/*.so $dir/*.a`; do
+# this is recursive
+for lib in `find $dir -name \*.a -or -name \*.so`; do
   if nm -C $dir/$lib | grep "$sym"; then
     echo "=> Found in lib $lib"
   fi
--- tritoncc/main.cpp
+++ tritoncc/main.cpp
@@ -1,9 +1,154 @@
 #include <iostream>
+#include <cassert>
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+
+#include "triton/Dialect/Triton/IR/Types.h"
+#include "triton/Dialect/Triton/IR/Dialect.h"
 
 int main(void) {
   mlir::MLIRContext ctx;
-  std::cout << "constructed an mlir::MLIRContext object " << &ctx << std::endl;
+  auto builder = std::make_unique<mlir::OpBuilder>(&ctx);
+  auto unkloc = builder->getUnknownLoc();
+  auto module = builder->create<mlir::ModuleOp>(unkloc);
+  auto function_name = "pointwise_fn";
+  int address_space = 1;
+  assert(!module.lookupSymbol(function_name));
+
+  ctx.loadDialect<mlir::triton::TritonDialect>();
+
+  auto i32ty = builder->getI32Type();
+  auto f32ty = builder->getF32Type();
+  auto f32pty = mlir::triton::PointerType::get(f32ty, address_space);
+
+  auto functy = builder->getFunctionType({f32pty, f32pty, f32pty, i32ty}, {}).dyn_cast<mlir::FunctionType>();
+
+  llvm::SmallVector<mlir::NamedAttribute> attrs = {
+    mlir::NamedAttribute(
+      builder->getStringAttr("sym_visibility"),
+      builder->getStringAttr("public")
+    ),
+    mlir::NamedAttribute(
+      builder->getStringAttr("noinline"),
+      builder->getBoolAttr(false)
+    )
+  };
+  auto funcop = builder->create<mlir::triton::FuncOp>(unkloc, function_name, functy, attrs);
+  module.push_back(funcop);
+
+  builder->setInsertionPointToStart(funcop.addEntryBlock());
+
+  auto pid = builder->create<mlir::triton::GetProgramIdOp>(unkloc, i32ty, mlir::triton::ProgramIDDimAttr::get(&ctx, mlir::triton::ProgramIDDim(0)));
+  auto range = builder->create<mlir::triton::MakeRangeOp>(unkloc,
+    mlir::RankedTensorType::get({32}, i32ty),
+    0, 32);
+
+  auto broadcasted = builder->createOrFold<mlir::triton::SplatOp>(
+    unkloc,
+    mlir::RankedTensorType::get({32}, i32ty),
+    builder->create<mlir::arith::MulIOp>(unkloc,
+      pid,
+      builder->create<mlir::arith::ConstantIntOp>(unkloc, 32, i32ty))
+  );
+
+  auto idx = builder->create<mlir::arith::AddIOp>(
+    unkloc,
+    range,
+    broadcasted
+  );
+
+  auto broadcasted_num = builder->createOrFold<mlir::triton::SplatOp>(
+    unkloc,
+    mlir::RankedTensorType::get({32}, i32ty),
+    funcop.getArgument(3)
+  );
+
+  auto mask = builder->create<mlir::arith::CmpIOp>(
+    unkloc,
+    mlir::arith::CmpIPredicate::slt,
+    idx,
+    broadcasted_num
+  );
+
+  // lhs_ptr
+  auto lhs_ptr = builder->createOrFold<mlir::triton::SplatOp>(
+    unkloc,
+    mlir::RankedTensorType::get({32}, f32pty),
+    funcop.getArgument(0)
+  );
+    
+  // load lhs
+  auto lhs = builder->create<mlir::triton::LoadOp>(
+    unkloc,
+    builder->create<mlir::triton::AddPtrOp>(
+      unkloc,
+      lhs_ptr.getType(),
+      lhs_ptr,
+      idx
+    ),
+    mask,
+    mlir::Value(),
+    mlir::triton::CacheModifier::NONE,
+    mlir::triton::EvictionPolicy::NORMAL,
+    false
+  );
+
+  // rhs_ptr
+  auto rhs_ptr = builder->createOrFold<mlir::triton::SplatOp>(
+    unkloc,
+    mlir::RankedTensorType::get({32}, f32pty),
+    funcop.getArgument(1)
+  );
+    
+  // load rhs
+  auto rhs = builder->create<mlir::triton::LoadOp>(
+    unkloc,
+    builder->create<mlir::triton::AddPtrOp>(
+      unkloc,
+      rhs_ptr.getType(),
+      rhs_ptr,
+      idx
+    ),
+    mask,
+    mlir::Value(),
+    mlir::triton::CacheModifier::NONE,
+    mlir::triton::EvictionPolicy::NORMAL,
+    false
+  );
+
+  // compute ans
+  auto ans = builder->create<mlir::arith::AddFOp>(
+    unkloc,
+    lhs,
+    rhs
+  );
+
+  // ans_ptr
+  auto ans_ptr = builder->createOrFold<mlir::triton::SplatOp>(
+    unkloc,
+    mlir::RankedTensorType::get({32}, f32pty),
+    funcop.getArgument(2)
+  );
+
+  // store ans
+  builder->create<mlir::triton::StoreOp>(
+    unkloc,
+    builder->create<mlir::triton::AddPtrOp>(
+      unkloc,
+      ans_ptr.getType(),
+      ans_ptr,
+      idx
+    ),
+    mask,
+    mlir::triton::CacheModifier::NONE,
+    mlir::triton::EvictionPolicy::NORMAL
+  );
+
+  builder->create<mlir::triton::ReturnOp>(unkloc);
+
+  module.dump();
+
   std::cout << "bye" << std::endl;
   return 0;
 }
