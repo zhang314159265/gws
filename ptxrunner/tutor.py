8affad66778a79d1d992365d98505f468aae1950
zhang314159265 <shunting@fb.com>
2024-03-03 23:52:13 -0800

    move dot.ptx from tritoncc to ptxrunner

--- ptxrunner/tutor.py
+++ ptxrunner/tutor.py
@@ -14,9 +14,17 @@ def sum_args_and_ref():
     ref = x.sum(dim=-1)
     return (x, torch.zeros_like(ref), 1024, 1024, ref), 1
 
+def dot_args_and_ref():
+    N = 32
+    a = torch.rand(N, N)
+    b = torch.rand(N, N)
+    ref = a @ b + 5
+    return (a, b, torch.zeros_like(ref), ref), 2
+
 for ptx_file, f_args_and_ref, gridDim, blockDim, shared in (
     ["add.ptx", add_args_and_ref, 32, 32*4, 0],
     ["sum.ptx", sum_args_and_ref, 512, 32*4, 2064],
+    ["dot.ptx", dot_args_and_ref, 1, 32*4, 8192],
 ):
     print(f"-> Start testing {ptx_file}")
     (*args, ref), act_idx = f_args_and_ref()
@@ -28,7 +36,7 @@ for ptx_file, f_args_and_ref, gridDim, blockDim, shared in (
     ptxrunner.load_and_run(
         ptx_code, args=args, gridDim=gridDim, blockDim=blockDim, shared=shared
     )
-    tol = {"atol": 1e-5, "rtol": 1e-5}
+    tol = {"atol": 1e-3, "rtol": 1e-3}
     assert torch.allclose(ref, act, **tol), f"ref:\n{ref}\nact:\n{act}"
     print(f"<- Done testing {ptx_file}")
 
