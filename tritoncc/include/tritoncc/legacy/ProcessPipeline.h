1500522c7846ab621ee0bfdbb6dcc7a0649ab45f
zhang314159265 <shunting@fb.com>
2024-04-22 22:43:55 -0700

    stage/make_ptx.h

--- tritoncc/include/tritoncc/legacy/ProcessPipeline.h
+++ tritoncc/include/tritoncc/legacy/ProcessPipeline.h
@@ -3,7 +3,7 @@
 #include "tritoncc/stage/make_ttir.h"
 #include "tritoncc/stage/make_ttgir.h"
 #include "tritoncc/stage/make_llir.h"
-#include "tritoncc/legacy/MakePTX.h"
+#include "tritoncc/stage/make_ptx.h"
 #include "tritoncc/legacy/MakeCubin.h"
 #include "tritoncc/legacy/Util.h"
 
@@ -13,7 +13,7 @@ std::string processPipeline(mlir::ModuleOp& M, Option& opt) {
   make_ttir(M);
   make_ttgir(M, opt);
   std::string src = make_llir(M, opt);
-  std::string ptxCode = makePTX(src, opt);
+  std::string ptxCode = make_ptx(src, opt);
   std::string cubinBytes = makeCubin(ptxCode, opt);
   return cubinBytes;
 }
