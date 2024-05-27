fd8eed6a1a3fee1488c742b43d674c97effd440b
zhang314159265 <shunting@fb.com>
2024-05-27 12:19:41 -0700

    follow mlir toy tutorial ch6

--- tutor/mlir-toy/include/toy/Pass.h
+++ tutor/mlir-toy/include/toy/Pass.h
@@ -3,11 +3,21 @@
 #include "llvm/ADT/SmallPtrSet.h"
 
 #include "mlir/Pass/Pass.h"
+#include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
+#include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
+#include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
+#include "mlir/Conversion/ArithToLLVM/ArithToLLVM.h"
+#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/BuiltinDialect.h"
 
 #define DEBUG_TYPE "shape-inference"
@@ -259,7 +269,7 @@ struct BinaryOpLowering : public mlir::ConversionPattern {
 using AddOpLowering = BinaryOpLowering<mlir::toy::AddOp, mlir::arith::AddFOp>;
 using MulOpLowering = BinaryOpLowering<mlir::toy::MulOp, mlir::arith::MulFOp>;
 
-struct PrintOpLowering : public mlir::OpConversionPattern<mlir::toy::PrintOp> {
+struct PrintOpLoweringPartial : public mlir::OpConversionPattern<mlir::toy::PrintOp> {
   using OpConversionPattern<mlir::toy::PrintOp>::OpConversionPattern;
 
   mlir::LogicalResult
@@ -324,7 +334,7 @@ struct ToyToAffineLoweringPass
         ConstantOpLowering,
         AddOpLowering,
         MulOpLowering,
-        PrintOpLowering,
+        PrintOpLoweringPartial,
         ReturnOpLowering,
         FuncOpLowering
     >(&getContext());
@@ -338,6 +348,158 @@ struct ToyToAffineLoweringPass
 
 std::unique_ptr<mlir::Pass> createLowerToAffinePass() {
   return std::make_unique<ToyToAffineLoweringPass>();
+}
+
+}
+
+namespace toy { // lowering to llvm dialect pass
+
+class PrintOpLoweringFull : public mlir::ConversionPattern {
+ public:
+  explicit PrintOpLoweringFull(mlir::MLIRContext *context)
+      : ConversionPattern(mlir::toy::PrintOp::getOperationName(), 1, context) {}
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::Operation *op, llvm::ArrayRef<mlir::Value> operands,
+      mlir::ConversionPatternRewriter &rewriter) const override {
+    auto *context = rewriter.getContext();
+    auto memRefType = llvm::cast<mlir::MemRefType>(*op->operand_type_begin());
+    auto memRefShape = memRefType.getShape();
+    auto loc = op->getLoc();
+
+    mlir::ModuleOp parentModule = op->getParentOfType<mlir::ModuleOp>();
+
+    // Get a symbol reference to the printf function, inserting it if necessary.
+    auto printfRef = getOrInsertPrintf(rewriter, parentModule);
+    mlir::Value formatSpecifierCst = getOrCreateGlobalString(
+      loc, rewriter, "frmt_spec", llvm::StringRef("%f \0", 4), parentModule);
+    mlir::Value newLineCst = getOrCreateGlobalString(
+      loc, rewriter, "nl", llvm::StringRef("\n\0", 2), parentModule);
+
+    // Create a loop for each of the dimensions within the shape.
+    llvm::SmallVector<mlir::Value, 4> loopIvs;
+    for (unsigned i = 0, e = memRefShape.size(); i != e; ++i) {
+      auto lowerBound = rewriter.create<mlir::arith::ConstantIndexOp>(loc, 0);
+      auto upperBound =
+          rewriter.create<mlir::arith::ConstantIndexOp>(loc, memRefShape[i]);
+      auto step = rewriter.create<mlir::arith::ConstantIndexOp>(loc, 1);
+      auto loop =
+          rewriter.create<mlir::scf::ForOp>(loc, lowerBound, upperBound, step);
+      for (mlir::Operation &nested : *loop.getBody()) {
+        rewriter.eraseOp(&nested);
+      }
+      loopIvs.push_back(loop.getInductionVar());
+
+      // Terminate the loop body.
+      rewriter.setInsertionPointToEnd(loop.getBody());
+
+      // Insert a newline after each of the inner dimensions of the shape.
+      if (i != e - 1) {
+        rewriter.create<mlir::LLVM::CallOp>(loc, getPrintfType(context), printfRef,
+            newLineCst);
+      }
+
+      rewriter.create<mlir::scf::YieldOp>(loc);
+      rewriter.setInsertionPointToStart(loop.getBody());
+    }
+
+    // Generate a call to printf for the current element of the loop.
+    auto printOp = llvm::cast<mlir::toy::PrintOp>(op);
+    auto elementLoad =
+        rewriter.create<mlir::memref::LoadOp>(loc, printOp.getInput(), loopIvs);
+    rewriter.create<mlir::LLVM::CallOp>(
+        loc, getPrintfType(context), printfRef,
+        llvm::ArrayRef<mlir::Value>({formatSpecifierCst, elementLoad}));
+
+    // Notifier the rewriter that this operation has been removed
+    rewriter.eraseOp(op);
+    return mlir::success();
+  }
+ private:
+  static mlir::LLVM::LLVMFunctionType getPrintfType(mlir::MLIRContext *context) {
+    auto llvmI32Ty = mlir::IntegerType::get(context, 32);
+    auto llvmPtrTy = mlir::LLVM::LLVMPointerType::get(context);
+    auto llvmFnType = mlir::LLVM::LLVMFunctionType::get(llvmI32Ty,
+        llvmPtrTy, /*isVarArg=*/true);
+    return llvmFnType;
+  }
+
+  static mlir::FlatSymbolRefAttr getOrInsertPrintf(mlir::PatternRewriter &rewriter,
+      mlir::ModuleOp module) {
+    auto *context = module.getContext();
+    if (module.lookupSymbol<mlir::LLVM::LLVMFuncOp>("printf")) {
+      return mlir::SymbolRefAttr::get(context, "printf");
+    }
+
+    // Insert the printf function into the body of the parent module
+    mlir::PatternRewriter::InsertionGuard insertGuard(rewriter);
+    rewriter.setInsertionPointToStart(module.getBody());
+    rewriter.create<mlir::LLVM::LLVMFuncOp>(module.getLoc(), "printf",
+        getPrintfType(context));
+    return mlir::SymbolRefAttr::get(context, "printf");
+  }
+
+  static mlir::Value getOrCreateGlobalString(
+      mlir::Location loc, mlir::OpBuilder &builder,
+      llvm::StringRef name, llvm::StringRef value,
+      mlir::ModuleOp module) {
+    mlir::LLVM::GlobalOp global;
+    if (!(global = module.lookupSymbol<mlir::LLVM::GlobalOp>(name))) {
+      mlir::OpBuilder::InsertionGuard insertGuard(builder);
+      builder.setInsertionPointToStart(module.getBody());
+      auto type = mlir::LLVM::LLVMArrayType::get(
+          mlir::IntegerType::get(builder.getContext(), 8), value.size());
+      global = builder.create<mlir::LLVM::GlobalOp>(
+        loc, type, /*isConstant=*/true,
+        mlir::LLVM::Linkage::Internal, name,
+        builder.getStringAttr(value),
+        /*alignment=*/0);
+    }
+
+    // Get the pointer to the first character in the global string.
+    mlir::Value globalPtr = builder.create<mlir::LLVM::AddressOfOp>(loc, global);
+    mlir::Value cst0 = builder.create<mlir::LLVM::ConstantOp>(
+        loc, builder.getI64Type(),
+        builder.getIndexAttr(0));
+
+    return builder.create<mlir::LLVM::GEPOp>(
+        loc, mlir::LLVM::LLVMPointerType::get(builder.getContext()),
+        global.getType(), globalPtr, llvm::ArrayRef<mlir::Value>({cst0, cst0}));
+  }
+};
+
+struct ToyToLLVMLoweringPass
+    : public mlir::PassWrapper<ToyToLLVMLoweringPass, mlir::OperationPass<mlir::ModuleOp>> {
+  void getDependentDialects(mlir::DialectRegistry &registry) const override {
+    registry.insert<mlir::LLVM::LLVMDialect, mlir::scf::SCFDialect>();
+  }
+
+  void runOnOperation() final {
+    mlir::LLVMConversionTarget target(getContext());
+    target.addLegalOp<mlir::ModuleOp>();
+
+    mlir::LLVMTypeConverter typeConverter(&getContext());
+
+    mlir::RewritePatternSet patterns(&getContext());
+    mlir::populateAffineToStdConversionPatterns(patterns);
+    mlir::populateSCFToControlFlowConversionPatterns(patterns);
+    mlir::arith::populateArithToLLVMConversionPatterns(typeConverter, patterns);
+    mlir::populateFinalizeMemRefToLLVMConversionPatterns(typeConverter, patterns);
+    mlir::cf::populateControlFlowToLLVMConversionPatterns(typeConverter, patterns);
+    mlir::populateFuncToLLVMConversionPatterns(typeConverter, patterns);
+
+    // lower toy.print
+    patterns.add<PrintOpLoweringFull>(&getContext());
+
+    auto module = getOperation();
+    if (failed(applyFullConversion(module, target, std::move(patterns)))) {
+      signalPassFailure();
+    }
+  }
+};
+
+std::unique_ptr<mlir::Pass> createLowerToLLVMPass() {
+  return std::make_unique<ToyToLLVMLoweringPass>();
 }
 
 }
--- tutor/mlir-toy/toyc.cpp
+++ tutor/mlir-toy/toyc.cpp
@@ -3,13 +3,22 @@
 #include "llvm/Support/ErrorOr.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h"
 
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/Parser/Parser.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/Passes.h"
 #include "mlir/Dialect/Affine/Passes.h"
+#include "mlir/Dialect/Func/Extensions/AllExtensions.h"
+#include "mlir/Dialect/LLVMIR/Transforms/Passes.h"
+#include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Export.h"
+#include "mlir/ExecutionEngine/ExecutionEngine.h"
+#include "mlir/ExecutionEngine/OptUtils.h"
 
 #include "toy/AST.h"
 #include "toy/Dump.h"
@@ -28,7 +37,14 @@ static cl::opt<std::string> inputFilename(
     cl::value_desc("filename"));
 
 namespace {
-enum Action { None, DumpAST, DumpMLIR };
+enum Action {
+  None,
+  DumpAST,
+  DumpMLIR,
+  DumpMLIRLLVM,
+  DumpLLVMIR,
+  RunJIT
+};
 enum InputType { Toy, MLIR };
 }
 
@@ -40,7 +56,12 @@ static cl::opt<enum InputType> inputType(
 static cl::opt<enum Action>
     emitAction("emit", cl::desc("Select the kind of output desired"),
     cl::values(clEnumValN(DumpAST, "ast", "output the AST dump")),
-    cl::values(clEnumValN(DumpMLIR, "mlir", "output the MLIR dump")));
+    cl::values(clEnumValN(DumpMLIR, "mlir", "output the MLIR dump")),
+    cl::values(clEnumValN(DumpMLIRLLVM, "mlir-llvm",
+        "output the MLIR dump after llvm lowering")),
+    cl::values(clEnumValN(DumpLLVMIR, "llvm", "output the LLVM IR dump")),
+    cl::values(clEnumValN(RunJIT, "jit",
+        "JIT the code and run it by involing the main function")));
 
 static cl::opt<bool> enableOpt("opt", cl::desc("Enable optimizations"));
 
@@ -102,21 +123,19 @@ int loadMLIR(llvm::SourceMgr &sourceMgr, mlir::MLIRContext &context,
   return 0;
 }
 
-int dumpMLIR() {
-  mlir::MLIRContext context;
+int loadAndProcessMLIR(mlir::MLIRContext &context, mlir::OwningOpRef<mlir::ModuleOp> &module) {
   // Load our Dialect in this MLIR Context.
   context.getOrLoadDialect<mlir::toy::ToyDialect>();
 
-  mlir::OwningOpRef<mlir::ModuleOp> module;
   llvm::SourceMgr sourceMgr; 
   if (int error = loadMLIR(sourceMgr, context, module)) {
     return error;
   }
 
+  mlir::PassManager pm(module.get()->getName());
+
   if (enableOpt) {
     {
-      mlir::PassManager pm(module.get()->getName());
-  
       // Inline all functions into main and then delete them.
       pm.addPass(mlir::createInlinerPass());
   
@@ -125,12 +144,8 @@ int dumpMLIR() {
       optPM.addPass(mlir::createCanonicalizerPass());
       optPM.addPass(mlir::createCSEPass());
   
-      if (mlir::failed(pm.run(*module))) {
-        return 4;
-      }
     }
     {
-      mlir::PassManager pm(module.get()->getName());
       pm.addPass(toy::createLowerToAffinePass());
 
       // Add a few cleanups post lowering.
@@ -140,14 +155,96 @@ int dumpMLIR() {
 
       optPM.addPass(mlir::affine::createLoopFusionPass());
       optPM.addPass(mlir::affine::createAffineScalarReplacementPass());
-
-      if (mlir::failed(pm.run(*module))) {
-        return 4;
-      }
     }
   }
 
-  module->dump();
+  bool isLoweringToLLVM = emitAction >= Action::DumpMLIRLLVM;
+
+  if (isLoweringToLLVM) {
+    pm.addPass(toy::createLowerToLLVMPass());
+    // This is necessary to have line tables emitted and basic
+    // debugger working. In the future we will add proper debug information
+    // emission directly from our frontend.
+    pm.addPass(mlir::LLVM::createDIScopeForLLVMFuncOpPass());
+  }
+
+  if (mlir::failed(pm.run(*module))) {
+    return 4;
+  }
+
+  return 0;
+}
+
+// LLVM dialect to LLVM IR
+int dumpLLVMIR(mlir::ModuleOp module) {
+  mlir::registerBuiltinDialectTranslation(*module->getContext());
+  mlir::registerLLVMDialectTranslation(*module->getContext());
+
+  // Convert the module to LLVM IR in a new LLVM IR context.
+  llvm::LLVMContext llvmContext;
+  auto llvmModule = mlir::translateModuleToLLVMIR(module, llvmContext);
+  if (!llvmModule) {
+    llvm::errs() << "Faield to emit LLVM IR\n";
+    return -1;
+  }
+
+  // Initialize LLVM targets.
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+
+  // Configure the LLVM module
+  auto tmBuilderOrError = llvm::orc::JITTargetMachineBuilder::detectHost();
+  if (!tmBuilderOrError) {
+    llvm::errs() << "Could not create JITTargetMachineBuilder\n";
+    return -1;
+  }
+
+  auto tmOrError = tmBuilderOrError->createTargetMachine();
+  if (!tmOrError) {
+    llvm::errs() << "Could not create TargetMachine\n";
+    return -1;
+  }
+  mlir::ExecutionEngine::setupTargetTripleAndDataLayout(
+      llvmModule.get(),
+      tmOrError.get().get());
+
+  // Optionally run an optimization pipeline over the llvm module.
+  auto optPipeline = mlir::makeOptimizingTransformer(
+      /*optLevel=*/enableOpt ? 3 : 0, /*sizeLevel=*/0,
+      /*targetMachine=*/nullptr);
+  if (auto err = optPipeline(llvmModule.get())) {
+    llvm::errs() << "Failed to optimize LLVM IR " << err << "\n";
+    return -1;
+  }
+  llvm::errs() << *llvmModule << "\n";
+  return 0;
+}
+
+int runJit(mlir::ModuleOp module) {
+  mlir::registerBuiltinDialectTranslation(*module->getContext());
+  mlir::registerLLVMDialectTranslation(*module->getContext());
+
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+
+  auto optPipeline = mlir::makeOptimizingTransformer(
+      /*optLevel=*/enableOpt ? 3 : 0, /*sizeLevel=*/0,
+      /*targetMachine=*/nullptr);
+
+  // Create an MLIR execution engine. The execution engine eagerly JIT-compiles
+  // the module.
+  mlir::ExecutionEngineOptions engineOptions;
+  engineOptions.transformer = optPipeline;
+  auto maybeEngine = mlir::ExecutionEngine::create(module, engineOptions);
+  assert(maybeEngine && "faield to construct an execution engine");
+  auto &engine = maybeEngine.get();
+
+  // Inoke the JIT-compiled function
+  auto invocationResult = engine->invokePacked("main");
+  if (invocationResult) {
+    llvm::errs() << "JIT invocation failed\n";
+    return -1;
+  }
   return 0;
 }
 
@@ -155,14 +252,32 @@ int main(int argc, char **argv) {
   cl::ParseCommandLineOptions(argc, argv, "toy compiler\n");
   // std::cerr << "inputFilename is " << inputFilename << std::endl;
   
-  switch (emitAction) {
-  case Action::DumpAST:
+  if (emitAction == Action::DumpAST) {
     return dumpAST();
-  case Action::DumpMLIR:
-    return dumpMLIR();
-  default:
-    llvm::errs() << "No action specified (parsing only?), use -emit=<action>\n";
   }
-  std::cerr << "bye" << std::endl;
+
+  mlir::DialectRegistry registry;
+  mlir::func::registerAllExtensions(registry);
+  mlir::MLIRContext context(registry);
+  mlir::OwningOpRef<mlir::ModuleOp> module;
+  if (int error = loadAndProcessMLIR(context, module)) {
+    return error;
+  }
+
+  bool isOutputingMLIR = emitAction <= Action::DumpMLIRLLVM;
+  if (isOutputingMLIR) {
+    module->dump();
+    return 0;
+  }
+
+  if (emitAction == Action::DumpLLVMIR) {
+    return dumpLLVMIR(*module);
+  }
+
+  if (emitAction == Action::RunJIT) {
+    return runJit(*module);
+  }
+
+  llvm::errs() << "No action specified (parsing only?), use -emit=<action>\n";
   return 0;
 }
