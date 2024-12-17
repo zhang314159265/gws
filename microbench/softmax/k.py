17287c5f8a702775d0a75548c56fb4555cfb03d1
zhang314159265 <shunting@fb.com>
2024-12-17 15:07:29 -0800

    update the saved softmax kernel gen by inductor to make it runnable again

--- microbench/softmax/k.py
+++ microbench/softmax/k.py
@@ -2,55 +2,55 @@
 import triton
 import triton.language as tl
 from torch._inductor.ir import ReductionHint
-from torch._inductor.ir import TileHint
-from torch._inductor.triton_heuristics import AutotuneHint, reduction
-from torch._inductor.utils import instance_descriptor
-from torch._inductor import triton_helpers
+from torch._inductor.runtime.triton_heuristics import AutotuneHint, reduction
+from torch._inductor.runtime import triton_helpers
 
 from torch._dynamo.testing import rand_strided
 from torch._C import _cuda_getCurrentRawStream as get_cuda_stream
 import torch
-from torch._inductor.triton_heuristics import grid
+from torch._inductor.runtime.triton_heuristics import grid
+from triton.compiler.compiler import AttrsDescriptor
+from torch._inductor.runtime.hints import AutotuneHint, ReductionHint, TileHint, DeviceProperties
 
 @reduction(
-    size_hints=[8192, 65536],
+    size_hints={'x': 8192, 'r0_': 65536},
     reduction_hint=ReductionHint.INNER,
     filename=__file__,
-    triton_meta={'signature': {0: '*fp32', 1: '*fp32', 2: 'i32', 3: 'i32'}, 'device': 0, 'device_type': 'cuda', 'constants': {}, 'configs': [instance_descriptor(divisible_by_16=(0, 1, 2, 3), equal_to_1=(), ids_of_folded_args=(), divisible_by_8=(2, 3))]},
-    inductor_meta={'autotune_hints': set(), 'kernel_name': 'triton_red_fused__softmax_0', 'mutated_arg_names': []}
+    triton_meta={'signature': {"in_ptr0": '*fp32', "out_ptr2": '*fp32', "xnumel": 'i32', "rnumel": 'i32'}, 'device': DeviceProperties(type='cuda', index=0, multi_processor_count=108, cc=80, major=8, regs_per_multiprocessor=65536, max_threads_per_multi_processor=2048, warp_size=32), 'constants': {}, 'configs': [AttrsDescriptor.from_dict({'arg_properties': {'tt.divisibility': (0, 1, 2, 3), 'tt.equal_to': ()}, 'cls': 'AttrsDescriptor'})]},
+    inductor_meta={'autotune_hints': set(), 'kernel_name': 'triton_red_fused__softmax_0', 'mutated_arg_names': [], "kernel_num_gb": 0}
 )
 @triton.jit
-def triton_red_fused__softmax_0(in_ptr0, out_ptr2, xnumel, rnumel, XBLOCK : tl.constexpr, RBLOCK : tl.constexpr):
+def triton_red_fused__softmax_0(in_ptr0, out_ptr2, xnumel, rnumel, XBLOCK : tl.constexpr, R0_BLOCK : tl.constexpr):
     xnumel = 8192
     rnumel = 65536
     xoffset = tl.program_id(0) * XBLOCK
     xindex = xoffset + tl.arange(0, XBLOCK)[:, None]
     xmask = xindex < xnumel
-    rbase = tl.arange(0, RBLOCK)[None, :]
+    rbase = tl.arange(0, R0_BLOCK)[None, :]
     x0 = xindex
-    _tmp2 = tl.full([XBLOCK, RBLOCK], float("-inf"), tl.float32)
-    for roffset in range(0, rnumel, RBLOCK):
+    _tmp2 = tl.full([XBLOCK, R0_BLOCK], float("-inf"), tl.float32)
+    for roffset in range(0, rnumel, R0_BLOCK):
         rindex = roffset + rbase
         rmask = rindex < rnumel
         r1 = rindex
         tmp0 = tl.load(in_ptr0 + (r1 + (65536*x0)), rmask, eviction_policy='evict_last', other=0.0)
-        tmp1 = tl.broadcast_to(tmp0, [XBLOCK, RBLOCK])
+        tmp1 = tl.broadcast_to(tmp0, [XBLOCK, R0_BLOCK])
         tmp3 = triton_helpers.maximum(_tmp2, tmp1)
         _tmp2 = tl.where(rmask, tmp3, _tmp2)
     tmp2 = triton_helpers.max2(_tmp2, 1)[:, None]
-    _tmp8 = tl.full([XBLOCK, RBLOCK], 0, tl.float32)
-    for roffset in range(0, rnumel, RBLOCK):
+    _tmp8 = tl.full([XBLOCK, R0_BLOCK], 0, tl.float32)
+    for roffset in range(0, rnumel, R0_BLOCK):
         rindex = roffset + rbase
         rmask = rindex < rnumel
         r1 = rindex
         tmp4 = tl.load(in_ptr0 + (r1 + (65536*x0)), rmask, eviction_policy='evict_last', other=0.0)
         tmp5 = tmp4 - tmp2
         tmp6 = tl.exp(tmp5)
-        tmp7 = tl.broadcast_to(tmp6, [XBLOCK, RBLOCK])
+        tmp7 = tl.broadcast_to(tmp6, [XBLOCK, R0_BLOCK])
         tmp9 = _tmp8 + tmp7
         _tmp8 = tl.where(rmask, tmp9, _tmp8)
     tmp8 = tl.sum(_tmp8, 1)[:, None]
-    for roffset in range(0, rnumel, RBLOCK):
+    for roffset in range(0, rnumel, R0_BLOCK):
         rindex = roffset + rbase
         rmask = rindex < rnumel
         r1 = rindex
@@ -81,11 +81,10 @@ def benchmark_all_configs(args):
 
 
 if __name__ == '__main__':
-    from torch._inductor.utils import get_num_bytes
-    from triton.testing import do_bench
+    from torch._inductor.runtime.benchmarking import benchmarker
 
     args = get_args()
-    ms = do_bench(lambda: call(args), rep=40, fast_flush=True)
-    num_gb = get_num_bytes(*args, num_in_out_args=0) / 1e9
+    ms = benchmarker.benchmark_gpu(lambda: call(args), rep=40)
+    num_gb = 0.0
     gb_per_s = num_gb / (ms / 1e3)
     print(f"{ms:.3f}ms    {num_gb:.3f}GB    {gb_per_s:.2f}GB/s")
