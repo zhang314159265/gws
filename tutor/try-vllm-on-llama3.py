da6cc44f060e63f7ff485ff960552e782acb78cf
Shunting Zhang <shunting@fb.com>
2025-07-16 15:25:58 -0700

    enhance the vllm tutorial

--- tutor/try-vllm-on-llama3.py
+++ tutor/try-vllm-on-llama3.py
@@ -1,11 +1,38 @@
+import torch
+from torch import nn
+from torch import distributed
+import os
+
+if os.getenv("PATCH_NN_MODULE_INIT") == "1":
+    orig_init = nn.Module.__init__
+    
+    def new_init(self, *args, **kwargs):
+        # distributed._DistributedPdb().set_trace()
+        print(f" -> init nn module {type(self)}")
+        return orig_init(self, *args, **kwargs)
+    
+    nn.Module.__init__ = new_init
+
 from vllm import LLM, SamplingParams
-from vllm.config import CompilationConfig
+from vllm.config import CompilationConfig, CompilationLevel
+
+# os.environ["VLLM_ENABLE_V1_MULTIPROCESSING"] = "0"
+
+weight_path = "/home/shunting/meta-llama/Meta-Llama-3.1-8B-Instruct"
 
 if __name__ == "__main__":
-    # XXX tFail when using the default CompilationConfig so far.
+    # XXX Fail when using the default CompilationConfig so far.
     cconfig = CompilationConfig(use_inductor=False)
-    llm = LLM(model="/home/shunting/meta-llama/Meta-Llama-3.1-8B-Instruct", compilation_config=cconfig)
+    llm = LLM(model=weight_path, compilation_config=cconfig)
     sampling_params = SamplingParams(temperature=0.7, max_tokens=32)
 
-    outputs = llm.generate(["Tell me a joke."], sampling_params)
-    print(outputs[0].outputs[0].text)
+    requests = [
+        "Tell me a joke.",
+        "How to estimate the value of pi in mathematics?",
+        "How does quicksort works?",
+    ]
+    outputs = llm.generate(requests, sampling_params)
+
+    assert len(outputs) == len(requests)
+    for i, req_text in enumerate(requests):
+        print(f"Response for request {i}: {outputs[i].outputs[0].text}") 
