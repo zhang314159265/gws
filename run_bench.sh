2b0ed594c842dfe5509bd1f98dd4d03cc637e3cb
Shunting Zhang <shunting@fb.com>
2025-09-03 11:28:37 -0700

    use cold-start-latency arg

--- run_bench.sh
+++ run_bench.sh
@@ -1,6 +1,8 @@
 # HF training
 python benchmarks/dynamo/huggingface.py --backend inductor --amp --performance --only BertForMaskedLM --training
 
+python benchmarks/dynamo/huggingface.py --backend inductor --amp --performance --only BertForMaskedLM --training --cold-start-latency
+
 # HF inference
 python benchmarks/dynamo/huggingface.py --performance --inference --bfloat16 --backend inductor --freezing --only BertForMaskedLM
 
