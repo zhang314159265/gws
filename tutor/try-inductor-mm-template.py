5aa653b95f2b709cd3d6d9e605dde551f97f56d1
Shunting Zhang <shunting@fb.com>
2025-09-12 14:57:17 -0700

    add epilogue to the mm

--- tutor/try-inductor-mm-template.py
+++ tutor/try-inductor-mm-template.py
@@ -5,8 +5,9 @@ inductor_config.max_autotune = True
 inductor_config.max_autotune_gemm_backends = "TRITON"
 
 @torch.compile
-def f(a, b):
-    return a @ b
+def f(a, b, c):
+    x = a @ b
+    return x * c
 
-x = torch.randn(1024, 1024, device="cuda")
-f(x, x)
+x, y, z = [torch.randn(1024, 1024, device="cuda") for _ in range(3)]
+f(x, y, z)
