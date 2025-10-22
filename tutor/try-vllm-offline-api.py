aa531bda971c009ec69797b9b0cc60a4f1b97f5f
Shunting Zhang <shunting@fb.com>
2025-10-22 00:04:05 -0700

    move around the vllm tutorial script

--- tutor/try-vllm-offline-api.py
+++ tutor/try-vllm-offline-api.py
@@ -3,26 +3,13 @@ from torch import nn
 from torch import distributed
 import os
 
-if os.getenv("PATCH_NN_MODULE_INIT") == "1":
-    orig_init = nn.Module.__init__
-    
-    def new_init(self, *args, **kwargs):
-        # distributed._DistributedPdb().set_trace()
-        print(f" -> init nn module {type(self)}")
-        return orig_init(self, *args, **kwargs)
-    
-    nn.Module.__init__ = new_init
-
 from vllm import LLM, SamplingParams
 from vllm.config import CompilationConfig, CompilationLevel
 
-# os.environ["VLLM_ENABLE_V1_MULTIPROCESSING"] = "0"
-model_name = "openai/gpt-oss-20b"
+model_name = "Qwen/Qwen3-0.6B"
 
 if __name__ == "__main__":
-    # XXX Fail when using the default CompilationConfig so far.
-    cconfig = CompilationConfig(use_inductor=False)
-    llm = LLM(model=model_name, compilation_config=cconfig)
+    llm = LLM(model=model_name)
     sampling_params = SamplingParams(temperature=0.7, max_tokens=32)
 
     requests = [
