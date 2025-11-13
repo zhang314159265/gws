1c5a6facad688b7ce826f735f827d039b84aaa5c
shunting314 <shunting@meta.com>
2025-11-12 22:47:07 -0800

    try out flex decoding

--- tutor/flex-attn.py
+++ tutor/flex-attn.py
@@ -2,18 +2,29 @@ import torch
 import torch.nn.functional as F
 from torch.nn.attention.flex_attention import flex_attention, create_block_mask
 import math
+import os
 
-flex_attention = torch.compile(flex_attention)
+COMPILE = os.environ.get("COMPILE", "1") == "1"
+USE_FLEX_DECODING = os.environ.get("USE_FLEX_DECODING", "1") == "1"
+
+if COMPILE:
+    flex_attention = torch.compile(flex_attention)
 
 def manual_attn(q, k, v):
+    if q.size(1) != k.size(1):
+        assert q.size(1) % k.size(1) == 0
+        ratio = q.size(1) // k.size(1)
+        B, _, S, D = k.shape
+        k = k[:, :, None, :, :].expand(-1, -1, ratio, -1, -1).reshape(B, -1, S, D)
+        v = v[:, :, None, :, :].expand(-1, -1, ratio, -1, -1).reshape(B, -1, S, D)
     score = q @ k.transpose(-1, -2)
 
     # scale
     score /= math.sqrt(q.shape[-1])
 
     # mask
-    S = q.shape[-2]
-    mask = torch.ones(S, S)
+    S1, S2 = q.shape[-2], k.shape[-2]
+    mask = torch.ones(S1, S2)
     mask = torch.tril(mask)
     score = torch.where(mask == 1, score, float('-inf'))
 
@@ -23,14 +34,14 @@ def manual_attn(q, k, v):
 def flex_attn_score_mod(q, k, v):
     def score_mod(score, b_idx, h_idx, q_idx, kv_idx):
         return torch.where(q_idx >= kv_idx, score, float("-inf"))
-    return flex_attention(q, k, v, score_mod=score_mod)
+    return flex_attention(q, k, v, score_mod=score_mod, enable_gqa=q.size(1) != k.size(1))
 
 def flex_attn_mask_mod(q, k, v):
     def mask_mod(b_idx, h_idx, q_idx, kv_idx):
         return q_idx >= kv_idx
-    S = q.shape[-2]
-    block_mask = create_block_mask(mask_mod, 1, 1, S, S)
-    return flex_attention(q, k, v, block_mask=block_mask)
+    S1, S2 = q.shape[-2], k.shape[-2]
+    block_mask = create_block_mask(mask_mod, 1, 1, S1, S2)
+    return flex_attention(q, k, v, block_mask=block_mask, enable_gqa=q.size(1) != k.size(1))
 
 def numeric_check(ref, act):
     correct = torch.allclose(ref, act, atol=1e-3, rtol=1e-3)
@@ -41,8 +52,11 @@ def numeric_check(ref, act):
 def main():
     B, H, S, D = 2, 4, 1024, 64
     q, k, v = [torch.randn(B, H, S, D) for _ in range(3)]
+    if USE_FLEX_DECODING:
+        q = torch.randn(B, H * 2, 4, D)
+
     ref = manual_attn(q, k, v)
-    numeric_check(ref, F.scaled_dot_product_attention(q, k, v, is_causal=True))
+    numeric_check(ref, F.scaled_dot_product_attention(q, k, v, is_causal=True, enable_gqa=q.size(1) != k.size(1)))
     numeric_check(ref, flex_attn_score_mod(q, k, v))
     numeric_check(ref, flex_attn_mask_mod(q, k, v))
     print("pass")
