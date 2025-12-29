f6af7cf99c2c47b0fddcf7c1a30d8510f100cba4
shunting314 <shunting@meta.com>
2025-12-29 11:21:06 -0800

    update linear-cel benchmarking script to do auto-chunking

--- tutor/liger-fused-linear-cross-entropy/tutor.py
+++ tutor/liger-fused-linear-cross-entropy/tutor.py
@@ -2,15 +2,35 @@ import sys
 import os
 from torch.nn import functional as F
 
-sys.path.append(os.path.dirname(__file__))
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
 import torch
 
 from torch import nn
-from utils import bench
+from triton.testing import do_bench
 
-sys.path.append("/home/shunting/ws/Liger-Kernel/src")
+def bench(f, name, warmup=5, profile_mem=False, profile=False):
+    for _ in range(warmup):
+        f()
+
+    if profile_mem:
+        torch.cuda.memory._record_memory_history()
+        f()
+        torch.cuda.memory._dump_snapshot(f"{name}.pickle")
+
+    if profile:
+        with torch.profiler.profile() as prof:
+            f()
+        path = f"/tmp/{name}.json"
+        prof.export_chrome_trace(path)
+        print(f"Profile trace writen to {path}")
+   
+    torch.cuda.reset_peak_memory_stats()
+    ms = do_bench(f)
+
+    print(f"{name}: {ms:.3f}ms")
+    print(f"Peak mem: {torch.cuda.max_memory_allocated() / 1e9:.3f} GB")
+    print()
+
+
 from liger_kernel.ops.fused_linear_cross_entropy import LigerFusedLinearCrossEntropyFunction
 
 torch.set_default_device("cuda")
@@ -36,13 +56,27 @@ def torchf(m, x, label):
     loss.backward()
     return loss
 
-opt_torchf = torch.compile(torchf)
+opt_torchf = torch.compile(torchf, options={"auto_chunker.enable": False})
+compiled_ligerf = torch.compile(ligerf, options={"auto_chunker.enable": False})
 
 expected = torchf(model, x, T).float()
 assert torch.allclose(expected, ligerf(model, x, T).float(), atol=1e-2, rtol=1e-2)
+assert torch.allclose(expected, compiled_ligerf(model, x, T).float(), atol=1e-2, rtol=1e-2)
 assert torch.allclose(expected, opt_torchf(model, x, T).float(), atol=1e-2, rtol=1e-2)
 
-bench(lambda: ligerf(model, x, T), "liger")
-bench(lambda: torchf(model, x, T), "torch")
-bench(lambda: opt_torchf(model, x, T), "opt_torch")
+bench(lambda: ligerf(model, x, T), "liger", profile=True)
+bench(lambda: compiled_ligerf(model, x, T), "compiled_liger", profile=True)
+bench(lambda: torchf(model, x, T), "torch-eager")
+bench(lambda: opt_torchf(model, x, T), "compile_no_chunking")
+
+for log_nchunk in range(2, 7):
+    torch._dynamo.reset()
+    nchunk = 2 ** log_nchunk
+    do_profile = nchunk == 64
+
+    # why num_chunk can not be ov
+    autochunker_torchf = torch.compile(torchf, options={"auto_chunker.enable": True, "auto_chunker.num_chunk": nchunk})
+    assert torch.allclose(expected, autochunker_torchf(model, x, T).float(), atol=1e-2, rtol=1e-2)
+    bench(lambda: autochunker_torchf(model, x, T), f"compile_{nchunk}_chunks", profile=do_profile)
+
 print("bye")
