e049525083a9bcb6858164fff2d0e5c5515ee123
shunting314 <shunting@meta.com>
2026-05-17 15:10:23 -0700

    sample.py

--- infer-from-scratch/sample.py
+++ infer-from-scratch/sample.py
@@ -1,13 +1,33 @@
 import torch
 
-def sample(logits, config):
-    if config.temperature == 0:
-        return logits.argmax().cpu().item()
-    # gumbel max:
-    # out-token = argmax(pi / qi)
-    # = argmax(log(pi) - log(qi))
-    # = argmax(xi - log(qi))
-    q = torch.empty_like(logits).exponential_()
-    return (logits / config.temperature - q.log()).argmax().cpu().item()
+# selector = "sample_multinomial"
+# selector = "sample_gumbel_max_with_softmax"
+selector = "sample_gumbel_max_without_softmax"
+
+def sample_multinomial(logits, temperature):
+    assert temperature > 0
+    probs = torch.softmax(logits / temperature, dim=-1)
+    return torch.multinomial(probs, 1).item()
+
+def sample_gumbel_max_with_softmax(logits, temperature):
+    assert temperature > 0
+    probs = torch.softmax(logits / temperature, dim=-1)
+    noise = torch.empty_like(probs).exponential_(1.0)
+    return (probs / noise).argmax().item()
+
+def sample_gumbel_max_without_softmax(logits, temperature):
+    assert temperature > 0
+    # argmax_i(exp(xi / temp) / sum_j(exp(xj / temp)) / exp_i)
+    # = argmax_i(exp(xi / temp) / exp_i)
+    # = argmax_i(xi / temp - log(exp_i))
+    noise = -torch.empty_like(logits).exponential_(1.0).log()
+    return (logits / temperature + noise).argmax().item()
 
 
+def sample(logits, config): 
+    if config.temperature == 0.0:
+        return logits.argmax().item()
+
+    if selector not in globals():
+        raise NotImplementedError(f"Unrecognized sampler selector: {selector}")
+    return globals()[selector](logits, config.temperature)
