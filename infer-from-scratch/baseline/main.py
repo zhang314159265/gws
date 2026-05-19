ce30131b0a9a36408c5b9202e2de01d0cae50470
shunting314 <shunting@meta.com>
2026-05-18 22:18:42 -0700

    config.py

--- infer-from-scratch/baseline/main.py
+++ infer-from-scratch/baseline/main.py
@@ -11,28 +11,7 @@ torch.manual_seed(1337)
 def print_once(msg):
     print(msg)
 
-class config:
-    checkpoint_file = "artifact/meta-llama/Meta-Llama-3-8B-Instruct/original/consolidated.00.pth"
-    tokenizer_file = "artifact/meta-llama/Meta-Llama-3-8B-Instruct/original/tokenizer.model"
-    vocab_size = 128256
-    num_layers = 32
-    num_q_heads = 32
-    num_kv_heads = 8
-    rms_norm_eps = 1e-5
-    hidden_size = 4096
-    intermediate_size = int(4096 * 3.5)
-    rope_theta = 500_000
-    temperature = 0.7
-    max_position_embeddings = 8192
-
-    # prompt = "Show me how quick-sort works."
-    # prompt = "What's the value of pi in mathematics?"
-    # prompt = "Can you explain FFT to me?"
-    # prompt = "Can you explain S&P index to me?"
-    # prompt = "Translate 'hello' to Chinese."
-    prompt = "Show me the C code for bubble sort."
-    # prompt = "Explain KL-divergence."
-
+from config import config
 from tokenizer import Tokenizer
 tokenizer = Tokenizer(config.tokenizer_file)
 
@@ -47,6 +26,7 @@ from generate import generate
 def interactive():
     while True:
         print("> ", end="")
+        torch.manual_seed(1337)
         prompt = input().strip()
         if not prompt:
             print("Done with the interactive mode")
