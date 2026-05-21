d6624d7a5c2461ba2ddf414b99fae2b1711d5fb0
shunting314 <shunting@meta.com>
2026-05-20 22:47:48 -0700

    rope.py

---
+++ infer-from-scratch/rope.py
@@ -0,0 +1,42 @@
+import torch
+
+"""
+Qwen rope uses a different convention compared to llama.
+For llama, q tensor is interpreted as:
+    real0, imag0, real1, imag1 ...
+For Qwen, q tensor is interpreted as:
+    real0, real1, ... imag0, imag1, ...
+"""
+
+class Rope:
+    cos = None
+    sin = None
+
+    @classmethod
+    def precompute_cis(cls, config):
+        head_dim = config.head_dim
+        half_head = head_dim // 2
+
+        a = torch.arange(0, config.max_position_embeddings)
+        b = torch.pow(config.rope_theta, -torch.arange(0, half_head).float() / half_head)
+        outer = torch.outer(a, b)
+        outer = torch.cat([outer, outer], dim=-1)
+        cls.cos = outer.cos().to(torch.bfloat16)
+        cls.sin = outer.sin().to(torch.bfloat16)
+
+    @classmethod
+    def rotate(cls, qk):
+        head_dim = qk.size(-1)
+        half_head = head_dim // 2
+
+        return torch.cat([
+            -qk[:, :, half_head:],
+            qk[:, :, :half_head],
+        ], dim=-1)
+
+    @classmethod
+    def apply(cls, qk, start_pos):
+        seqlen, _, _ = qk.shape
+
+        return (qk * cls.cos[start_pos: start_pos + seqlen, None, :]
+            + cls.rotate(qk) * cls.sin[start_pos: start_pos + seqlen, None, :])
