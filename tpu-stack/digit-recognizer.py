4ba047b7dd6085d4f234ea9760636acfdfafd6ef
shunting <zhangst1986@gmail.com>
2026-07-26 21:35:24 -0700

    jit compile digit recognizer

--- tpu-stack/digit-recognizer.py
+++ tpu-stack/digit-recognizer.py
@@ -1,5 +1,10 @@
-# 95.47% accuracy using adamw after 10 epochs
-# 93.23% accuracy using sgd after 10 epochs
+# Accuracy:
+#   95.47% accuracy using adamw after 10 epochs
+#   93.23% accuracy using sgd after 10 epochs
+# Training latency
+#   without jax.jit, abount 5.5 seconds per epoch
+#   jax.jit train_step, 0.35 seconds per epoch (15.7x speedup)
+#   also jax.jit predict, 0.17 seconds per epoch (32.4x speedup)
 
 from tensorflow.keras.datasets import mnist
 import jax
@@ -7,6 +12,7 @@ import jax.numpy as jnp
 import flax.linen as nn
 from flax.training import train_state
 import optax
+import time
 
 def display_sample(x, y):
     import matplotlib.pyplot as plt
@@ -61,16 +67,30 @@ def compute_loss(params, x, label):
     loss = optax.softmax_cross_entropy_with_integer_labels(logits, label).mean()
     return loss
 
+@jax.jit
+def predict(state, x, label):
+    logits = model.apply({"params": state.params}, x)
+    prediction = jnp.argmax(logits, axis=1)
+    return (prediction == label).sum()
+
+
 def compute_accuracy():
     tot = 0
     cor = 0
     for x, label in data_loader("test"):
-        logits = model.apply({"params": state.params}, x)
-        prediction = jnp.argmax(logits, axis=1)
-        tot += prediction.shape[0]
-        cor += (prediction == label).sum()
+        tot += x.shape[0]
+        cor += predict(state, x, label)
 
     return cor / tot if tot > 0 else 0
+
+@jax.jit
+def train_step(state, x, label):
+    loss, grads = jax.value_and_grad(lambda params: compute_loss(params, x, label))(
+        state.params,
+    )
+    state = state.apply_gradients(grads=grads)
+    return state, loss
+
 
 model = Model()
 key = jax.random.key(23)
@@ -84,12 +104,11 @@ state = train_state.TrainState.create(
 )
 
 for epoch_id in range(nepoch):
+    start_ts = time.perf_counter()
     for x, label in data_loader("train"):
-        loss, grads = jax.value_and_grad(lambda params: compute_loss(params, x, label))(
-            state.params,
-        )
-        state = state.apply_gradients(grads=grads)
+        state, loss = train_step(state, x, label)
     acc = compute_accuracy()
-    print(f"epoch {epoch_id}: accuracy {acc * 100:.2f}%")
+    elapse = time.perf_counter() - start_ts
+    print(f"epoch {epoch_id}: accuracy {acc * 100:.2f}%, elapse {elapse:.3f} seconds")
 
 print("Done")
